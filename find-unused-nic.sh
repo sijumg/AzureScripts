fb37a43dd45d56bd54cf8bd2b4f1d64cdd8051d8
sijumg <50022305+sijumg@users.noreply.github.com>
2021-10-01 19:08:37 -0400

    Create find-unused-nic.sh

---
+++ find-unused-nic.sh
@@ -0,0 +1,17 @@
+# Set deleteUnattachedNics=1 if you want to delete unattached NICs
+# Set deleteUnattachedNics=0 if you want to see the Id(s) of the unattached NICs
+deleteUnattachedNics=0
+
+unattachedNicsIds=$(az network nic list --query '[?virtualMachine==`null`].[id]' -o tsv)
+for id in ${unattachedNicsIds[@]}
+do
+   if (( $deleteUnattachedNics == 1 ))
+   then
+
+       echo "Deleting unattached NIC with Id: "$id
+       az network nic delete --ids $id
+       echo "Deleted unattached NIC with Id: "$id
+   else
+       echo $id
+   fi
+done
